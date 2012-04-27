e7d237be8b9a7dec7f77fba7f94ccf765c5bd94d
Han Jing <hanjing@berkeley.edu>
2012-04-27 01:17:43 -0700

    finished part 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,10 +12,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    url(r'^admin/', include(admin.site.urls)),
+    # Uncomment the next line to enable the admin:  
+    url(r'^polls/', include('polls.urls')),
+    url (r'^admin/', include(admin.site.urls)),
 )
